0f21b4e330b8aad94f32532e0148cb8924b72e28
Artur Lima <artursantosj@gmail.com>
2020-04-07 19:05:03 -0300

    Js_If and Else

---
+++ exercicios/javascript/ex005.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrpassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicios/javascript/ex006.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Top!')
+} else {
+    console.log('Você não é tão top assim!')
+}
